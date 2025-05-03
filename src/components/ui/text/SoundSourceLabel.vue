599c3b60bb7c75e14bbca7b0901abb791be6f39a
Zaria Burton <zaria.burton2000@gmail.com>
2025-05-03 00:42:54 -0400

    working on sound source label

---
+++ src/components/ui/text/SoundSourceLabel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="absolute pointer-events-none w-20 overflow-hidden text-xs text-center text-neutral-700 dark:text-neutral-300"
+    :style="{
+      top: `${y}px`,
+      left: `${x}px`,
+      transform: 'translate(-50%, 0)',
+    }"
+  >
+    <Vue3Marquee :pauseOnHover="true" :duration="6" class="whitespace-nowrap">
+      {{ text }}
+    </Vue3Marquee>
+  </div>
+</template>
+
+<script setup>
+import { Vue3Marquee } from 'vue3-marquee'
+
+defineProps({
+  text: String,
+  x: Number,
+  y: Number
+})
+</script>
