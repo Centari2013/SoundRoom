2729d9bdd069c2500e77a38cb6f53c5c4210dc04
Zaria Burton <zaria.burton2000@gmail.com>
2025-06-26 21:12:14 -0400

    refactor: remove unused listener prop from SidebarRight component

--- src/components/SoundRoom/SidebarRight.vue
+++ src/components/SoundRoom/SidebarRight.vue
@@ -1,11 +1,11 @@
 <template>
   <aside class="bg-neutral-100 dark:bg-neutral-900 border-l border-neutral-300 dark:border-neutral-800 p-4 space-y-4">
-    <SelectedSourcePanel v-bind="{ listener, actionManager, selectedSource }" />
+    <SelectedSourcePanel v-bind="{ actionManager, selectedSource }" />
   </aside>
 </template>
 
 <script setup>
 import SelectedSourcePanel from '@/components/ui/panels/SelectedSourcePanel.vue'
 
-defineProps(['selectedSource', 'listener', 'actionManager'])
+defineProps(['selectedSource', 'actionManager'])
 </script>
