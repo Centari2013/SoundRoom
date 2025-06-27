f75225790087f37312bf6a2e16d6014ce7044244
Zaria Burton <zaria.burton2000@gmail.com>
2025-06-26 20:01:05 -0400

    refactor: streamline props binding in SidebarRight component for improved clarity

--- src/components/SoundRoom/SidebarRight.vue
+++ src/components/SoundRoom/SidebarRight.vue
@@ -1,6 +1,6 @@
 <template>
   <aside class="bg-neutral-100 dark:bg-neutral-900 border-l border-neutral-300 dark:border-neutral-800 p-4 space-y-4">
-    <SelectedSourcePanel :listener="listener" :actionManager="actionManager" :selectedSource="selectedSource" />
+    <SelectedSourcePanel v-bind="{ listener, actionManager, selectedSource }" />
   </aside>
 </template>
 
