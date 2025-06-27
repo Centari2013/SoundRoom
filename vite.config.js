b5633aa634fc0f2cc2d63e44b50dac53e6a6d89b
Zaria Burton <zaria.burton2000@gmail.com>
2025-06-27 06:43:50 -0400

    chore: remove vercel configuration file and update vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+const isGitHubPages = process.env.DEPLOY_TARGET === 'GH_PAGES';
+
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss()
-],
+  ],
   resolve: {
     alias: [
       {
         find: '@',
         replacement: '/src'
-      },
+      }
     ]
   },
-  base: '/SoundRoom/'
+  base: isGitHubPages ? '/SoundRoom/' : '/'
 })
