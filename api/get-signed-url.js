f5ad68fde9afcb74fae6834eb1afb5206d6f1f4c
Zaria Burton <zaria.burton2000@gmail.com>
2025-07-02 09:03:08 -0400

    fix: update CORS headers to allow requests from any origin

--- api/get-signed-url.js
+++ api/get-signed-url.js
@@ -18,7 +18,7 @@ export async function GET(request) {
     return new Response("Missing 'key' query param", { 
       status: 400,
       headers: {
-        "Access-Control-Allow-Origin": "http://localhost:4000",
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
         "Access-Control-Allow-Credentials": "true",
