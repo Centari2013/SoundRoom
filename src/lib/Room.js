1bc90c2698e663a57dc3809c5e2ee655dcfea437
Zaria Burton <zaria.burton2000@gmail.com>
2025-07-13 12:36:38 -0400

    Introduce central Room class

--- src/lib/Room.js
+++ src/lib/Room.js
@@ -1,10 +1,19 @@
 // lib/Room.js
 
+import AudioEngine from './AudioEngine'
+import Listener from './Listener'
+
 export default class Room {
   width = 0
   height = 0
   name = ''
   id = null
+  /** @type {AudioEngine|null} */
+  audioEngine = null
+  /** @type {Listener|null} */
+  listener = null
+  /** @type {Array<any>} */
+  soundSources = []
 
   /**
    * Create a new room instance.
@@ -19,6 +28,61 @@ export default class Room {
     this.height = height
     this.name = name
     this.id = id
+    this.audioEngine = new AudioEngine([])
+    this.listener = new Listener()
+    this.soundSources = []
+  }
+
+  /**
+   * Assign an existing AudioEngine instance to this room.
+   * @param {AudioEngine} engine
+   */
+  setAudioEngine(engine) {
+    this.audioEngine = engine
+  }
+
+  /**
+   * Assign a listener instance to this room.
+   * @param {Listener} listener
+   */
+  setListener(listener) {
+    this.listener = listener
+  }
+
+  /**
+   * Add a sound source to the room and inform the source about its new context.
+   * @param {any} source
+   */
+  addSoundSource(source) {
+    if (!source) return
+    if (typeof source.setRoom === 'function') {
+      source.setRoom(this)
+    }
+    this.soundSources.push(source)
+  }
+
+  /**
+   * Remove a sound source from the room.
+   * @param {any} source
+   */
+  removeSoundSource(source) {
+    const i = this.soundSources.indexOf(source)
+    if (i !== -1) this.soundSources.splice(i, 1)
+  }
+
+  /**
+   * Update the room dimensions and notify sound sources.
+   * @param {number} width
+   * @param {number} height
+   */
+  updateRoomSize(width, height) {
+    this.width = width
+    this.height = height
+    this.soundSources.forEach(s => {
+      if (typeof s.onRoomResize === 'function') {
+        s.onRoomResize(width, height)
+      }
+    })
   }
 
   /**
