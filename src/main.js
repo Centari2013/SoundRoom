5535c0fdc0755eb6e0f8f2ac91eb7a9214fde3db
Zaria Burton <zaria.burton2000@gmail.com>
2025-11-04 15:20:31 -0500

    Remove sendDefaultPii option from Sentry initialization

--- src/main.js
+++ src/main.js
@@ -16,7 +16,6 @@ Sentry.init({
 
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/vue/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
 
   integrations: [
   ],
